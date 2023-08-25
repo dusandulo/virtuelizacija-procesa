5521ba4bcb26513b373b75b09c41635903d3600f
dusandulo <borovicanindusan@gmail.com>
2023-08-26 00:10:18 +0200

    Kreirana klasa Load

---
+++ virtuelizacija-procesa/Common/Load.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Load
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public double ForecastValue { get; set; }
+        public double MeasuredValue { get; set; }
+        public double AbsolutePercentageDeviation { get; set; }
+        public double SquaredDeviation { get; set; }
+        public int ImportedFileId { get; set; }
+
+
+        public Load()
+        {
+
+        }
+
+        public Load(int id, DateTime timeStamp, double forecastValue, double measuredValue,
+            double absolutePercentageDeviation, double squaredDeviation, int importedFileId)
+        {
+            Id = id;
+            TimeStamp = timeStamp;
+            ForecastValue = forecastValue;
+            MeasuredValue = measuredValue;
+            AbsolutePercentageDeviation = absolutePercentageDeviation;
+            SquaredDeviation = squaredDeviation;
+            ImportedFileId = importedFileId;
+        }
+    }
+}
