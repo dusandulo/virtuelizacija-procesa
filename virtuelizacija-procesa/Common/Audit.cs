c502716936dfc1d725ae81ca1847a73054277db4
dusandulo <borovicanindusan@gmail.com>
2023-08-26 00:17:22 +0200

    Dodata enumeracija za tip poruka i klasa Audit

---
+++ virtuelizacija-procesa/Common/Audit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Audit
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public MessageType MessageType { get; set; }
+        public string Message { get; set; }
+
+        public Audit()
+        {
+
+        }
+        public Audit(int id, DateTime timeStamp, MessageType messageType, string message)
+        {
+            Id = id;
+            TimeStamp = timeStamp;
+            MessageType = messageType;
+            Message = message;
+        }
+
+    }
+}
