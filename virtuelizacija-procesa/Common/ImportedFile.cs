48cc06b4f540d203ccb85f413062aa6ff9f6175e
dusandulo <borovicanindusan@gmail.com>
2023-08-26 00:12:52 +0200

    Kreirana klasa ImportedFile

---
+++ virtuelizacija-procesa/Common/ImportedFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ImportedFile
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+
+        public ImportedFile()
+        {
+
+        }
+        
+        public ImportedFile(int id, string fileName)
+        {
+            Id = id;
+            FileName = fileName;
+        }
+
+    }
+}
