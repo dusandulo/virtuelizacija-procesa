56b19e1ce42a0e89f1eab2c6362058c751c6ed01
dusandulo <borovicanindusan@gmail.com>
2023-08-27 22:39:08 +0200

    .

--- virtuelizacija-procesa/Common/Audit.cs
+++ virtuelizacija-procesa/Common/Audit.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
 {
+    [DataContract]
     public class Audit
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public DateTime TimeStamp { get; set; }
+        [DataMember]
         public MessageType MessageType { get; set; }
+        [DataMember]
         public string Message { get; set; }
 
         public Audit()
--- virtuelizacija-procesa/Common/Load.cs
+++ virtuelizacija-procesa/Common/Load.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
 {
+    [DataContract]
     public class Load
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public DateTime TimeStamp { get; set; }
+        [DataMember]
         public double ForecastValue { get; set; }
+        [DataMember]
         public double MeasuredValue { get; set; }
+        [DataMember]
         public double AbsolutePercentageDeviation { get; set; }
+        [DataMember]
         public double SquaredDeviation { get; set; }
+        [DataMember]
         public int ImportedFileId { get; set; }
 
 
@@ -32,6 +41,14 @@ namespace Common
             AbsolutePercentageDeviation = absolutePercentageDeviation;
             SquaredDeviation = squaredDeviation;
             ImportedFileId = importedFileId;
+        }
+
+        public Load(int id, DateTime timeStamp, double forecastValue, double measuredValue)
+        {
+            Id = id;
+            TimeStamp = timeStamp;
+            ForecastValue = forecastValue;
+            MeasuredValue = measuredValue;
         }
     }
 }
